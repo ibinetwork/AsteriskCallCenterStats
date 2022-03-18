ca52748c300f5bbd4c88bb5d883d8864a98b98b5
ibinetwork <rafael@ibinetwork.com.br>
2022-03-17 21:56:05 -0300

    pt-br
    Português Brasileiro

---
+++ lang/br.php
@@ -0,0 +1,224 @@
+<?php
+/*
+   Copyright 2017, https://asterisk-pbx.ru
+   
+   This file is part of Asterisk Call Center Stats.
+    Asterisk Call Center Stats is free software: you can redistribute it 
+    and/or modify it under the terms of the GNU General Public License as 
+    published by the Free Software Foundation, either version 3 of the 
+    License, or (at your option) any later version.
+
+    Asterisk Call Center Stats is distributed in the hope that it will be 
+    useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Asterisk Call Center Stats.  If not, see 
+    <http://www.gnu.org/licenses/>.
+*/
+
+$dayp[0] = "Domingo";
+$dayp[1] = "Segunda";
+$dayp[2] = "Terça";
+$dayp[3] = "Quarta";
+$dayp[4] = "Quinta";
+$dayp[5] = "Sexta";
+$dayp[6] = "Sabado";
+
+$yearp[0] = "Janeiro";
+$yearp[1] = "Fevereiro";
+$yearp[2] = "Мarço";
+$yearp[3] = "Аbril";
+$yearp[4] = "Мaio";
+$yearp[5] = "Junho";
+$yearp[6] = "Julho";
+$yearp[7] = "Аgosto";
+$yearp[8] = "Setembro";
+$yearp[9] = "Оutubro";
+$yearp[10]= "Novembro";
+$yearp[11]= "Dezembro";
+
+// Menu options
+$lang['br']['menu_home']         = "Relatórios";
+$lang['br']['menu_answered']     = "Atendidas";
+$lang['br']['menu_unanswered']   = "Perdidas";
+$lang['br']['menu_distribution'] = "Distribuição";
+
+// tooltips
+$lang['br']['pdfhelp'] = "Exportar os dados para PDF";
+$lang['br']['csvhelp'] = "Exportar os dados em CSV";
+$lang['br']['gotop']   = "Ir para to topo da página";
+
+// Index page
+$lang['br']['ALL']               = "Todos";
+$lang['br']['ALLS']              = "Todos";
+$lang['br']['lower']             = "Menor  ...";
+$lang['br']['higher']            = "Maior  ...";
+$lang['br']['select_queue']      = "Selecionar Fila";
+$lang['br']['select_agent']      = "Selecionar Agente";
+$lang['br']['select_timeframe']  = "Selecionar Periodo";
+$lang['br']['queue']   	         = "Fila";
+$lang['br']['start']   	         = "Inicio";
+$lang['br']['end']   	         = "Final";
+$lang['br']['display_report']    = "Exibir Relatório";
+$lang['br']['shortcuts']         = "Atalhos";
+$lang['br']['today']             = "Hoje";
+$lang['br']['this_week']         = "Esta semana";
+$lang['br']['this_month']        = "Este mês";
+$lang['br']['last_three_months'] = "Últimos 3 meses";
+$lang['br']['available']         = "Disponível";
+$lang['br']['selected']          = "Selecionado";
+$lang['br']['invaliddate']       = "Periodo invalido";
+
+// Answered page
+$lang['br']['answered_calls_by_agent'] = "Chamadas atendidas por agentes";
+$lang['br']['agent_in_call_dur'] = "Tempo total do agente em atendimento (min)";
+$lang['br']['answered_calls_by_queue'] = "Chamadas atendidas por fila";
+$lang['br']['anws_unanws_by_hour']     = "Atendidas/Perdidas por hora";
+$lang['br']['report_info']       = "Informações do Relatório";
+$lang['br']['period']            = "Periodo";
+$lang['br']['answered_calls']    = "Chamadas Atendidas";
+$lang['br']['transferred_calls'] = "Chamadas Transferidas";
+$lang['br']['secs']              = "segs";
+$lang['br']['minutes']           = "min";
+$lang['br']['hours']             = "hs";
+$lang['br']['calls']             = "chamadas";
+$lang['br']['Calls']             = "Chamadas";
+$lang['br']['agent']             = "Аgente";
+$lang['br']['avg']               = "Média";
+$lang['br']['avg_calltime']      = "Duração média";
+$lang['br']['avg_holdtime']      = "Tempo de espera médio";
+$lang['br']['percent']           = "%";
+$lang['br']['total']             = "Total";
+$lang['br']['calltime']          = "Tempo das chamadas";
+$lang['br']['holdtime']          = "Tempo de espera";
+$lang['br']['total_time_agent']  = "Tempo total por agente (segs)";
+$lang['br']['no_calls_agent']    = "Numero de chamadas por agente";
+$lang['br']['call_response']     = "Nível do Serviço";
+$lang['br']['within']            = "Com ";
+$lang['br']['answer']            = "Atendidas";
+$lang['br']['count']             = "Quantidade";
+$lang['br']['call_abandon']      = "Chamadas Abandonadas";
+$lang['br']['recordfile']        = "Gravação";
+$lang['br']['dur_by_period']     = "Tempo por periodo";
+$lang['br']['delta']             = "Delta";
+$lang['br']['disconnect_cause']  = "Motivo do término da chamada";
+$lang['br']['cause']             = "Causa";
+$lang['br']['agent_hungup']      = "Аgente desligou";
+$lang['br']['caller_hungup']     = "Cliente desligou";
+$lang['br']['caller']            = "Cliente";
+$lang['br']['transfers']         = "Transferencias";
+$lang['br']['to']                = "Para";
+//
+$lang['br']['0-5sec']            = "0-5 seg";
+$lang['br']['6-10sec']           = "6-10 seg";
+$lang['br']['11-15sec']          = "11-15 seg";
+$lang['br']['16-20sec']          = "16-20 seg";
+$lang['br']['21-25sec']          = "21-25 seg";
+$lang['br']['26sec']             = "26+ seg";
+$lang['br']['0-25sec']           = "0-25 seg";
+
+$lang['br']['10sec']             = "0-10 seg";
+$lang['br']['20sec']             = "10-20 seg";
+$lang['br']['30sec']             = "20-30 seg";
+$lang['br']['40sec']             = "30-40 seg";
+$lang['br']['50sec']             = "40-50 seg";
+$lang['br']['60sec']             = "50-60 seg";
+$lang['br']['61sec']             = "61+ seg";
+$lang['br']['time']              = "Hora";
+$lang['br']['event']             = "Evento";
+$lang['br']['callid']            = "Uniqueid";
+$lang['br']['qcallid']           = "Uniqueid da Fila";
+$lang['br']['page']              = "Pagina";
+$lang['br']['of']                = "de";
+//
+$lang['br']['15sec']             = "15 seg";
+$lang['br']['_30sec']            = "30 seg";
+$lang['br']['45sec']             = "45 seg";
+$lang['br']['_60sec']            = "60 seg";
+$lang['br']['75sec']             = "75 seg";
+$lang['br']['90sec']             = "90 seg";
+$lang['br']['91sec']             = "91+ seg";
+// Unanswered page
+$lang['br']['unanswered_calls']    = "Chamadas Perdidas";
+$lang['br']['number_unanswered']   = "Numero de chamadas perdidas";
+$lang['br']['avg_wait_before_dis'] = "Média de tempo de espera antes de desligar";
+$lang['br']['avg_queue_pos_at_dis']= "Média de posição na antes de desligar";
+$lang['br']['avg_queue_start']     = "Posição média na fila de espera";
+$lang['br']['user_abandon']        = "Abandono do cliente";
+$lang['br']['abandon']             = "Abandono";
+$lang['br']['timeout']             = "Тempo limite da chamada";
+$lang['br']['unanswered_calls_qu'] = "Não respondidas por filas";
+$lang['br']['unanswered_by_period_all'] = "Distribuição total de falhas por periodo (sem tempo limite):";
+$lang['br']['unanswered_by_period_queue'] = "Distribuição de falhas no período de espera por filas (sem tempo limite):";
+$lang['br']['hangupposition'] = "Posição de saída";
+$lang['br']['enterposition'] = "Posição de entrada";
+$lang['br']['user_abandon_calls'] = "Cliente Abandonou";
+$lang['br']['user_abandon_calls_timeout'] = "Детализация: Таймаут";
+
+// Distribution
+$lang['br']['totals']              = "Totais";
+$lang['br']['number_answered']     = "Numero de chamadas atendidas";
+$lang['br']['number_unanswered']   = "Numero de chamadas perdidas";
+$lang['br']['agent_login']         = "Login de agente";
+$lang['br']['agent_logoff']        = "Logout de agente";
+$lang['br']['call_distrib_day']    = "Distribuição de chamadas por dia";
+$lang['br']['call_distrib_hour']   = "Distribuição de chamadas por hora";
+$lang['br']['call_distrib_week']   = "Distribuição de chamadas por dia da semana";
+$lang['br']['date']                = "Data";
+$lang['br']['day']                 = "Dia";
+$lang['br']['days']                = "Dias";
+$lang['br']['hour']                = "Hora";
+$lang['br']['answered']            = "Atendida";
+$lang['br']['unanswered']          = "Perdida";
+$lang['br']['percent_answered']    = "% Atendida";
+$lang['br']['percent_unanswered']  = "% Perdida";
+$lang['br']['login']               = "Login";
+$lang['br']['logoff']              = "Logoff";
+$lang['br']['answ_by_day']         = "Chamadas atendidas por dia da semana";
+$lang['br']['unansw_by_day']       = "Chamadas perdidas por dia da semana";
+$lang['br']['avg_call_time_by_day']= "Duração média de chamadas por dia da semana";
+$lang['br']['avg_hold_time_by_day']= "Duração média de espera por dia da semana";
+$lang['br']['answ_by_hour']        = "Chamadas atendidas por hora";
+$lang['br']['unansw_by_hour']      = "Chamadas perdidas por hora";
+$lang['br']['avg_call_time_by_hr'] = "Tempo médio de chamada por hora";
+$lang['br']['avg_hold_time_by_hr'] = "Tempo médio de espera por hora";
+$lang['br']['page']                = "Página";
+$lang['br']['export']              = "Exportar tabela:";
+
+$lang['br']['server_time']          = "Hora do Servidor:";
+$lang['br']['php_parsed']           = "Servidor atualizou esta pagina em ";
+$lang['br']['seconds']              = "segundos";
+$lang['br']['current_agent_status'] = "Dashboard - Agentes em Tempo Real";
+$lang['br']['hide_loggedoff']       = "Ocultar agente não logado";
+$lang['br']['agent_status']         = "Estado do Agente";
+$lang['br']['state']                = "Estado";
+$lang['br']['durat']                = "Duração";
+$lang['br']['clid']                 = "Numero de origem";
+$lang['br']['last_in_call']         = "Última ligação à";
+$lang['br']['not_in_use']           = "Disponivel";
+$lang['br']['paused']               = "Em Pausa";
+$lang['br']['busy']                 = "Ocupado";
+$lang['br']['unavailable']          = "Indisponivel";
+$lang['br']['unknown']              = "Desconhecido";
+$lang['br']['dialout']              = "Efetuando chamada";
+$lang['br']['no_info']              = "Nenhuma informação disponível";
+$lang['br']['min_ago']              = "minutos atrás";
+$lang['br']['queue_summary']        = "Resumo da Fila";
+$lang['br']['staffed']              = "Аgentes";
+$lang['br']['talking']              = "Falando";
+$lang['br']['paused']               = "Em Pausa";
+$lang['br']['calls_waiting']        = "Chamadas aguardando";
+$lang['br']['oldest_call_waiting']  = "Chamada em espera mais antiga";
+$lang['br']['calls_waiting_detail'] = "Detalhes das chamada aguardando";
+$lang['br']['position']             = "Posição";
+$lang['br']['callerid']             = "Numero de origem";
+$lang['br']['wait_time']            = "Tempo aguardando";
+//cdr
+$lang['br']['cdr']                  = "RAW";
+$lang['br']['filter']               = "Filtro";
+$lang['br']['page_rows']            = "Linhas por pagina:";
+$lang['br']['hidden_ringnoanswer']  = "Ocultar RINGNOANSWER";
+
+?>
